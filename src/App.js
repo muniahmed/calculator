c89b172acf85969f0602d30076cb30595bbd121c
muniahmed <ahmed.muni.98@gmail.com>
2022-06-27 14:59:06 -0400

    Added equal button functionality

--- src/App.js
+++ src/App.js
@@ -3,20 +3,30 @@ import ButtonGrid from "./components/ButtonGrid";
 import Display from "./components/Display";
 import React, { useState } from "react";
 
+
 function App() {
   const [userInput, setUserInput] = useState("");
   const [inputMemory, setInputMemory] = useState("");
   const [isDecimal, setIsDecimal] = useState(false);
+  const [answered, setAnswered] = useState(false);
 
 
   function handleNumber(number) {
-    setUserInput(userInput.toString() + number.toString())
+    if (answered) {
+      setUserInput(number.toString());
+      setAnswered(false);
+    } else if (isNaN(userInput)) {
+      setUserInput(number.toString());
+    } else {
+      setUserInput(userInput.toString() + number.toString())
+    }
   }
 
   function handleOperator(operator) {
     setInputMemory(inputMemory + Number(userInput) + operator);
     setUserInput("");
     setIsDecimal(false);
+    setAnswered(false);
   }
 
   function handleDecimal() {
@@ -24,21 +34,45 @@ function App() {
       setIsDecimal(true);
       setUserInput(userInput + ".");
     }
+
   }
+
+  // function evil(equation) {
+  //   return new Function('return ' + equation)();
+  // }
+
+
 
   function handleEquals() {
-    console.log(inputMemory);
+    let equation;
+    if (answered) {
+      setUserInput(0);
+      setAnswered(false);
+      return;
+    }
+    // eslint-disable-next-line eqeqeq
+    else if (userInput == 0 || userInput == "" || isNaN(userInput)) {
+      equation = inputMemory.slice(1, -1)
+      setInputMemory(equation);
+    } else {
+      equation = inputMemory + Number(userInput)
+      setInputMemory(equation);
+    }
 
+    // eslint-disable-next-line no-eval
+    let answer = eval(equation.replace(/[^-()\d/*+.]/g, ''));
+    setUserInput(answer);
     setInputMemory("");
-    setUserInput("");
-
+    setAnswered(true);
   }
+
 
   function clearDisplay() {
     setUserInput("");
     setInputMemory("");
     setIsDecimal(false);
   }
+
 
   return (
     <div className="App">
